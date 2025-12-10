b2e6f83f25ac371dd4baecd46ece349ce5cef84f
Arshad Rafey Siddiqui <arshadsiddiqui153@gmail.com>
2025-12-11 02:04:04 +0530

    fix: Explicitly set REACT_APP_API_URL during frontend build

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r backend/requirements.txt
 
 # Install frontend dependencies and build
 npm install --prefix frontend
-npm run build --prefix frontend
+(cd frontend && REACT_APP_API_URL=http://web-env-v2-env.eba-jb352ukt.ap-south-1.elasticbeanstalk.com/ npm run build)
